29b8b47c98617079ae3fb0b2953c9794cfac9538
Jose Pina <pinajoma0@gmail.com>
2021-11-18 11:45:56 +0100

    wallet ready

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 3. yarn add @web3-react/core web3-react
 4. yarn add web3
 5. yarn add @chakra-ui/react @emotion/react @emotion/styled framer-motion
+6. yarn add @web3-react/injected-connector
+7. yarn add @chakra-ui/icons
--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import { Routes, Route } from"react-router-dom";
 import Home from"./views/home";
-import { useEffect } from "react";
-import Web3 from "web3";
+// import { useEffect } from "react";
+// import Web3 from "web3";
+import MainLayout from "./layouts/main";
 
 function App() {
-  useEffect(() => {
-    if( window.ethereum ) {
-      // window.ethereum
-      // .request({
-      //   method:"eth_requestAccounts",
-      // })
-      // .then((accounts) => console.log(accounts))
+  // useEffect(() => {
+  //   if( window.ethereum ) {
+  //     // window.ethereum
+  //     // .request({
+  //     //   method:"eth_requestAccounts",
+  //     // })
+  //     // .then((accounts) => console.log(accounts))
 
-      const web3 = new Web3(window.ethereum);
-      web3.eth.requestAccounts().then(console.log);
-    }
+  //     const web3 = new Web3(window.ethereum);
+  //     web3.eth.requestAccounts().then(console.log);
+  //   }
 
-  },[]);
+  // },[]);
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Home />} />
+    <MainLayout>
+      <Routes>
+        <Route path="/" element={<Home />} />
       </Routes>
+    </MainLayout>
     </>
   );
 }
